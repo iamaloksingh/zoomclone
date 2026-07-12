cf38d5091e6b1d4e554292dd564805b564a0ac39
Alok Singh <aloksinghvirendraji@gmail.com>
2026-07-12 13:22:31 +0530

    error

--- backend/src/app.js
+++ backend/src/app.js
@@ -41,14 +41,20 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options(/(.*)/, cors(corsOptions));
 app.use((req, res, next) => {
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Origin", req.header("Origin") || "");
+    const origin = req.headers.origin;
+
+    if (origin && allowedOrigins.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
+        res.header("Access-Control-Allow-Credentials", "true");
         res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
         res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-        return res.sendStatus(200);
     }
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(204);
+    }
+
     next();
 });
 
